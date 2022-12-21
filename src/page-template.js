bae479bcc9184cd0ed741c146718fb6b89c288e7
[Sarah Lang] <[lynnlang98@gmail.com]>
2022-12-20 19:06:27 -0500

    generateHTML refactored

--- src/page-template.js
+++ src/page-template.js
@@ -1,6 +1,68 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Engineer = require('../lib/Engineer');
+let addEmployee;
+const teamMembers = [];
+let starterHTML = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>`
+const endingHTML = `<script src="https://kit.fontawesome.com/23474bb623.js" crossorigin="anonymous"></script>
+</body>
+</html>`;
+
+const prompt = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Employee name?',
+  },
+  {
+    type: 'list',
+    message: 'What role does this employee play?',
+    choices: ['Manager', 'Engineer', 'Intern'],
+    name: 'role'
+  },
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: 'Enter Manager office phone number',
+    when: (answer) => answer['role'] === 'Manager',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter Engineer GitHub Username',
+    when: (answer) => answer['role'] === 'Engineer',
+  },
+  {
+    type: 'input',
+    name: 'school',
+    message: 'Name of school Intern attends?',
+    when: (answer) => answer['role'] === 'Intern',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "What is this Employee's ID number?",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Employee's email?",
+  },
+  {
+    type: 'list',
+    message: 'Do you want to add another Employee?',
+    choices: ['Yes!', 'No!'],
+    name: 'continue'
+  },
+]
 
 employeeRole = role => {
   switch (role) {
@@ -18,117 +80,60 @@ employeeRole = role => {
   }
 }
 
-renderPosition = role => {
-    switch(role) {
-        case 'Manager':
-            return `<li class="list-group-item">Office Number: ${officeNumber}</li>`
-            break;
+renderPosition = ({ role, officeNumber, github, school }) => {
+  switch (role) {
+    case 'Manager':
+      return `<li class="list-group-item text-dark">Office Number: ${officeNumber}</li>`
+      break;
 
-        case 'Engineer':
-          return `<li class="list-group-item">GitHub: ${github}</li>`
-          break;
+    case 'Engineer':
+      return `<li class="list-group-item text-dark">GitHub: ${github}</li>`
+      break;
 
-        case 'Intern':
-          return `<li class="list-group-item">Employee School: ${school}</li>`
-          break;
-}}
+    case 'Intern':
+      return `<li class="list-group-item text-dark">Employee School: ${school}</li>`
+      break;
+  }
+}
 
-// function renderManager() {
-//   if (role === 'Manager') {
-//     return `<li class="list-group-item">Office Number: ${officeNumber}</li>`
-//   } else {
-//     return '';
-//   }
-// }
-
-// function renderEngineer() {
-//   if (role === 'Engineer') {
-//     return `<li class="list-group-item">GitHub: ${github}</li>`
-//   } else {
-//     return '';
-//   }
-// }
-
-// function renderInter() {
-//   if (role === 'Intern') {
-//     return `<li class="list-group-item">Employee School: ${school}</li>`
-//   } else {
-//     return '';
-//   }
-// }
-
-const generateHTML = ({ name, role, email, id }) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
+function generateHTML(teamMembers) {
+  const team = teamMembers.map((member) => {
+    const { name, email, id, role, position } = member;
+    return `<div class="bg-danger text-center jumbotron display-4">My Team</div>
   <div class="card bg-primary text-white m-2" style="width: 300px;">
   <div class="container">
     <h1 class="display-4">${name}</h1>
-    ${employeeRole(role)}
+    ${role}
     <ul class="list-group list-group-flush">
-      ${renderPosition(role)}
-      <li class="list-group-item">Email: ${email}</li>
-      <li class="list-group-item">Employee Id: ${id}</li>
+      ${position}
+      <li class="list-group-item text-dark">Email: ${email}</li>
+      <li class="list-group-item text-dark">Employee Id: ${id}</li>
     </ul>
   </div>
-</div>
-<script src="https://kit.fontawesome.com/23474bb623.js" crossorigin="anonymous"></script>
-</body>
-</html>`;
+</div>`;
+  });
+  generateHTML(team);
+};
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Employee name?',
-    },
-    {
-      type: 'list',
-      message: 'What role does this employee play?',
-      choices: ['Manager', 'Engineer', 'Intern'],
-      name: 'role'
-    },
-    {
-      type: 'input',
-      name: 'id',
-      message: "What is this Employee's ID number?",
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: "Employee's email?",
-    },
-    // {
-    //   type: 'input',
-    //   name: 'github',
-    //   message: 'Enter Engineer GitHub Username',
-    // },
-    // {
-    //   type: 'input',
-    //   name: 'school',
-    //   message: 'Name of school Intern attends?',
-    // },
-  ])
+do {
+  inquirer
+    .prompt(prompt)
+    .then((answers) => {
+      const role = employeeRole(answers.role)
+      const position = renderPosition(answers)
+      const htmlPageContent = generateHTML(answers, role, position);
+      starterHTML += htmlPageContent;
 
-  
-  .then((answers) => {
-    if (answers.role === 'Manager') {
-      prompt.next({
-        type: 'input',
-        name: 'officeNumber',
-        message: 'Enter Manager office phone number',
-      })
-    }
-    const htmlPageContent = generateHTML(answers);
-    
-  fs.writeFile('index.html', htmlPageContent, (err) =>
-    err ? console.log(err) : console.log('Successfully created index.html!')
-  );
-});
+      if (answers.continue === 'Yes!') {
+        addEmployee = true;
+        teamMembers.push(answers);
+      } else {
+        addEmployee = false;
+        const finalHTML = starterHTML + endingHTML;
+        fs.writeFile('index.html', finalHTML, (err) =>
+          err ? console.log(err) : console.log('Successfully created index.html!')
+        );
+      }
+      console.info(addEmployee)
+    });
+} while (addEmployee)
